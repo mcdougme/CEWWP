f09dfdcd9c057a3a2121f9a77285c1e57aada774
Melissa McDougall <mcdougme@umich.edu>
2015-12-14 10:24:49 -0500

    initialize repository

---
+++ assets/js/jquery-min.js
@@ -0,0 +1,11 @@
+var headerHeight = 200;
+
+$(window).bind('scroll', function () {
+if ($(window).scrollTop() > headerHeight) {
+    $('#myNav').removeClass('navbar-top');
+    $('#myNav').addClass('navbar-fixed-top');
+} else {
+    $('#myNav').removeClass('navbar-fixed-top');
+    $('#myNav').addClass('navbar-top');
+}
+});  
