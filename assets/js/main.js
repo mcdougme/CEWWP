cfb9c12b91d17d29b6c1fe37b881269e55b0361e
Melissa McDougall <mcdougme@umich.edu>
2015-12-16 15:35:15 -0500

    yuri fixes pt 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,11 @@ if ($(window).scrollTop() > headerHeight) {
     $('#myNav').removeClass('navbar-fixed-top');
     $('#myNav').addClass('navbar-top');
 }
-});  
+});
+
+jQuery('.toggle-nav, .menu a').click(function(e) {
+    jQuery('.toggle-nav').toggleClass('active');
+    jQuery('.menu ul').toggleClass('active');
+
+    e.preventDefault();
+});
