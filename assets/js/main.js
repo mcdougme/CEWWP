1f66b1b095741e5dc1c46146296d9debfbca785b
Melissa McDougall <mcdougme@umich.edu>
2016-01-31 01:04:55 -0500

    add pages and shit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-var headerHeight = 200;
-
-$(window).bind('scroll', function () {
-if ($(window).scrollTop() > headerHeight) {
-    $('#myNav').removeClass('navbar-top');
-    $('#myNav').addClass('navbar-fixed-top');
-} else {
-    $('#myNav').removeClass('navbar-fixed-top');
-    $('#myNav').addClass('navbar-top');
-}
-});
-
 jQuery('.toggle-nav, .menu a').click(function(e) {
     jQuery('.toggle-nav').toggleClass('active');
     jQuery('.menu ul').toggleClass('active');
